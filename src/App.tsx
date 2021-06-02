5c75f4a177f4944f43e86fc2d0824a430ffc39db
Jamie Popkin <popkinj@littleearth.ca>
2021-06-02 16:01:34 -0700

    Testing offline plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import 'leaflet/dist/leaflet.css';
+import PouchDB from 'pouchdb-browser';
 import {
   MapContainer,
-  TileLayer
+  TileLayer,
+  LayersControl
 } from 'react-leaflet';
+import PouchDBTileLayer from "react-leaflet-pouchdb-tilelayer";
 import './App.css';
 
 function App() {
@@ -13,9 +16,19 @@ function App() {
       zoom={5}
       style={{height: '100%'}}
     >
-      <TileLayer
-        url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-      />
+      <LayersControl position='topright'>
+        <LayersControl.BaseLayer checked name="Regular Layer">
+          <TileLayer
+            url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+          />
+        </LayersControl.BaseLayer>
+        {/* <LayersControl.BaseLayer name="Pouch Offline Layer">
+          <PouchDBTileLayer
+            url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}'
+            crossOrigin={true}
+          />
+        </LayersControl.BaseLayer> */}
+      </LayersControl>
     </MapContainer>
   );
 }
